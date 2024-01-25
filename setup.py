dac29629c645b04142e59afabbb06fcd26dc3ce7
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-01-25 15:16:06 +0100

    Enable install without cloning

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='ppiformer',
     version='1.0',
     packages=find_packages(),
+    install_requires=required
 )
